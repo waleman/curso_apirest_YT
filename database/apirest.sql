a19893bc49065fdc301c141ad3010d5ebf318368
Wilfredo Aleman <w.aleman.hn@gmail.com>
2020-08-07 02:29:38 +0200

    Creamos la base de datos - Inicio del curso

---
+++ database/apirest.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-08-2020 a las 02:16:48
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.2.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `apirest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `CitaId` int(11) NOT NULL,
+  `PacienteId` varchar(45) DEFAULT NULL,
+  `Fecha` varchar(45) DEFAULT NULL,
+  `HoraInicio` varchar(45) DEFAULT NULL,
+  `HoraFIn` varchar(45) DEFAULT NULL,
+  `Estado` varchar(45) DEFAULT NULL,
+  `Motivo` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacientes`
+--
+
+CREATE TABLE `pacientes` (
+  `PacienteId` int(11) NOT NULL,
+  `DNI` varchar(45) DEFAULT NULL,
+  `Nombre` varchar(150) DEFAULT NULL,
+  `Direccion` varchar(45) DEFAULT NULL,
+  `CodigoPostal` varchar(45) DEFAULT NULL,
+  `Telefono` varchar(45) DEFAULT NULL,
+  `Genero` varchar(45) DEFAULT NULL,
+  `FechaNacimiento` date DEFAULT NULL,
+  `Correo` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `UsuarioId` int(11) NOT NULL,
+  `Usuario` varchar(45) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  `Estado` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_token`
+--
+
+CREATE TABLE `usuarios_token` (
+  `TokenId` int(11) NOT NULL,
+  `UsuarioId` varchar(45) DEFAULT NULL,
+  `Token` varchar(45) DEFAULT NULL,
+  `Estado` varchar(45) CHARACTER SET armscii8 DEFAULT NULL,
+  `Fecha` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`CitaId`);
+
+--
+-- Indices de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`PacienteId`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`UsuarioId`);
+
+--
+-- Indices de la tabla `usuarios_token`
+--
+ALTER TABLE `usuarios_token`
+  ADD PRIMARY KEY (`TokenId`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `CitaId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  MODIFY `PacienteId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios_token`
+--
+ALTER TABLE `usuarios_token`
+  MODIFY `TokenId` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
