8629d911355284f6d91c117fc62efc753340f93d
Wilfredo Aleman <w.aleman.hn@gmail.com>
2020-08-10 17:33:16 +0200

    video - #3 Terminando la clase coneixon

--- clases/conexion/conexion.php
+++ clases/conexion/conexion.php
@@ -36,6 +36,51 @@ class conexion {
     }
 
 
+    private function convertirUTF8($array){
+        array_walk_recursive($array,function(&$item,$key){
+            if(!mb_detect_encoding($item,'utf-8',true)){
+                $item = utf8_encode($item);
+            }
+        });
+        return $array;
+    }
+
+
+    public function obtenerDatos($sqlstr){
+        $results = $this->conexion->query($sqlstr);
+        $resultArray = array();
+        foreach ($results as $key) {
+            $resultArray[] = $key;
+        }
+        return $this->convertirUTF8($resultArray);
+
+    }
+
+
+
+    public function nonQuery($sqlstr){
+        $results = $this->conexion->query($sqlstr);
+        return $this->conexion->affected_rows;
+    }
+
+
+    public function nonQueryId($sqlstr){
+        $results = $this->conexion->query($sqlstr);
+         $filas = $this->conexion->affected_rows;
+         if($filas >= 1){
+            return $this->conexion->insert_id;
+         }else{
+             return 0;
+         }
+    }
+
+
+
+
+
+
+
+
 
      
 
--- index.php
+++ index.php
@@ -3,5 +3,9 @@ require_once "clases/conexion/conexion.php";
 
 $conexion = new conexion;
 
+$query = "Select * from pacientes";
+
+print_r($conexion->obtenerDatos($query));
+
 
 ?>
